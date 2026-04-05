a951f0e27a04cf0571cc1002558af1e01271c183
root <root@1438685-01.novalocal>
2026-04-05 19:43:00 +0700

    Tach 3 file va fix loi trang chu 404

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
+const path = require('path'); 
 const axios = require('axios'); 
 const app = express();
 const port = 3000;
 
-// Cấu hình để Express tự động tìm và phục vụ file index.html, main.js, 1.png ở cùng thư mục
 app.use(express.static(__dirname));
 
 const CONTAINER_NAME = process.env.CONTAINER_NAME || "Backend_Unknown";
 
-// 1. API LẤY LỊCH SỬ KLINE
 app.get('/api/history', async (req, res) => {
     try {
         const symbol = req.query.symbol || 'PAXGUSDT'; 
@@ -29,7 +28,6 @@ app.get('/api/history', async (req, res) => {
     }
 });
 
-// 2. API LẤY GIÁ REALTIME
 app.get('/api/gold', async (req, res) => {
     try {
         const symbol = req.query.symbol || 'PAXGUSDT';
@@ -42,6 +40,10 @@ app.get('/api/gold', async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: "Lỗi tải giá" });
     }
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => console.log(`API chay cong ${port}`));
